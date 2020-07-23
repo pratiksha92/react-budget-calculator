097c51b4035ab973c3f8aa117d833304a0ecc6bb
Pratiksha Bhangdiya <pratiksha@Amols-MacBook-Air.local>
2020-07-22 17:27:47 -0700

    react budget calculator

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+
+export const ExpenseList = ({
+  expenses,
+  handleEdit,
+  handleDelete,
+  clearItems,
+}) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return (
+            <Item
+              key={expense.id}
+              expense={expense}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            ></Item>
+          );
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+          clear expenses <MdDelete className="btn-icon"></MdDelete>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ExpenseList;
